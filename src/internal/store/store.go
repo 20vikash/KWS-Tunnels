f63e57a8611f46cb78fac8a81c7ea03b4f6c1f01
ssvikash_20 <arisivikash@gmail.com>
2025-10-03 11:07:01 +0530

    Update the storage struct

--- src/internal/store/store.go
+++ src/internal/store/store.go
@@ -1,15 +1,20 @@
 package store
 
 import (
+	"context"
+
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/redis/go-redis/v9"
 )
 
 type Storage struct {
 	TunnelStore interface {
+		ValidateTunnelFromUID(ctx context.Context, uid int, tunnel string) (bool, error)
+		GetDomainFromTunnel(ctx context.Context, tunnelName string, uid int) (string, error)
 	}
 
 	InMemoryStore interface {
+		GetUidFromSecret(ctx context.Context, secret string) (int, error)
 	}
 }
 
