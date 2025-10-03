4554311a212040b2652f92801417af3fc68143c2
ssvikash_20 <arisivikash@gmail.com>
2025-10-03 10:20:10 +0530

    Get UID from secret(redis)

--- src/internal/store/inmemory.go
+++ src/internal/store/inmemory.go
@@ -1,7 +1,22 @@
 package store
 
-import "github.com/redis/go-redis/v9"
+import (
+	"context"
+	"log"
+
+	"github.com/redis/go-redis/v9"
+)
 
 type RedisStore struct {
 	db *redis.Client
+}
+
+func (rs *RedisStore) GetUidFromSecret(ctx context.Context, secret string) (int, error) {
+	val, err := rs.db.Get(ctx, secret).Int()
+	if err != nil {
+		log.Println("Failed to get tunnel UID int")
+		return -1, err
+	}
+
+	return val, nil
 }
